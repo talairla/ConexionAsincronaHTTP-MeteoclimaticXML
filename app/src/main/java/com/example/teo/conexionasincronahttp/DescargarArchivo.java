d5d18aa2b4d9b1936b717f78a827d397b81098b0
Teo <talairla@educa.aragon.es>
2017-01-18 02:46:16 +0100

    Capturando  datos

--- app/src/main/java/com/example/teo/conexionasincronahttp/DescargarArchivo.java
+++ app/src/main/java/com/example/teo/conexionasincronahttp/DescargarArchivo.java
@@ -1,8 +1,15 @@
 package com.example.teo.conexionasincronahttp;
 
 import android.os.AsyncTask;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -10,6 +17,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 /**
  * Created by Teo on 16/01/2017.
@@ -33,9 +44,48 @@ public class DescargarArchivo extends AsyncTask<String, Void, String> {
     }
 
     @Override
-    protected void onPostExecute(String salida) {
-        super.onPostExecute(salida);
+    protected void onPostExecute(String xml) {
+        super.onPostExecute(xml);
+
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = null;
+        try {
+            db = dbf.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+        ByteArrayInputStream bis = new ByteArrayInputStream(xml.getBytes());
+        Document doc = null;
+        try {
+            doc = db.parse(bis);
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        doc.getDocumentElement().normalize();
+        Element rootElement = doc.getDocumentElement();
+        String id = doc.getElementsByTagName("id").item(0).getTextContent();
+        NodeList temperaturas = doc.getElementsByTagName("temperature").item(0).getChildNodes();
+        String salida ="";
+        for (int i=0;i<temperaturas.getLength();i++){
+            temperaturas.item(i).normalize();
+            salida = salida +temperaturas.item(i).getNodeName()+": "+temperaturas.item(i).getTextContent();
+        }
         tvSalida.setText(salida);
+    }
+
+    protected String getString(String tagName, Element element) {
+        NodeList list = element.getElementsByTagName(tagName);
+        if (list != null && list.getLength() > 0) {
+            NodeList subList = list.item(0).getChildNodes();
+
+            if (subList != null && subList.getLength() > 0) {
+                return subList.item(0).getNodeValue();
+            }
+        }
+
+        return null;
     }
 
     private String descargaUrl(String param) throws IOException{
--- app/src/main/java/com/example/teo/conexionasincronahttp/MainActivity.java
+++ app/src/main/java/com/example/teo/conexionasincronahttp/MainActivity.java
@@ -1,20 +1,41 @@
 package com.example.teo.conexionasincronahttp;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String tag = "DescargaAsyn";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnDescarga = (Button) findViewById(R.id.btnDescargar);
+        btnDescarga.setOnClickListener(this);
     }
 
-    public void descargarFichero(View view) {
+    @Override
+    public void onClick(View v) {
         TextView tvSalida = (TextView) findViewById(R.id.tvDownloadedFile);
-        tvSalida.append("\n Hola!");
+        tvSalida.setMovementMethod(new ScrollingMovementMethod());
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        EditText etURL = (EditText) findViewById(R.id.etURL);
+        String urlString = etURL.getText().toString();
+
+        if(networkInfo != null  && networkInfo.isConnected()){
+            Log.i(tag, "Descargando archivo");
+            DescargarArchivo descargarArchivo = new DescargarArchivo(tvSalida);
+            descargarArchivo.execute(urlString);
+
+        }
     }
 }
